909fb47f8afae0d53d243b62392c22ce6f13a4a5
fstipo <fstipanov@gmail.com>
2022-05-04 10:11:49 +0200

    useState with object

--- src/components/UseStateComp.js
+++ src/components/UseStateComp.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 
+// ako je početno stanje neka složena matematička operacija - da nam ne uspori aplikaciju
+// const init = () => {
+//   console.log('render');
+//   return 4;
+// };
+
 const UseStateComp = () => {
-  const [count, setCount] = useState(4);
-  const decreaseCountHandler = () => {
-    setCount((prevCount) => prevCount - 1);
-    setCount((prevCount) => prevCount - 1);
-  };
-  const increaseCountHandler = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+  const [countState, setState] = useState({ count: 4, theme: 'red' });
+  const count = countState.count;
+  const theme = countState.theme;
+
+  //with objects
+  const decreaseCountHandler = () =>
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 1 };
+    });
+
+  const increaseCountHandler = () =>
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
 
   return (
     <div className="text-center mt-4">
@@ -19,6 +31,7 @@ const UseStateComp = () => {
         -
       </button>
       <span className="fw-bold">{count}</span>
+      <span className="fw-bold">{theme}</span>
       <button
         className="btn btn-danger px-4 ms-4"
         onClick={increaseCountHandler}
