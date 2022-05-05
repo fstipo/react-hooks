9fb28d22b6793c8e3633ec6866e52b34497e9edc
fstipo <fstipanov@gmail.com>
2022-05-05 13:34:16 +0200

    React rooter

---
+++ src/components/Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Page.css';
+
+const Page = ({ title }) => {
+  return (
+    <>
+      <h2 className="my-5">{title}</h2>
+      <section className="rounded">aaaaa</section>
+    </>
+  );
+};
+
+export default Page;
